cb0e2fb3484957a956f63f9723234f3a3fddc9e4
Omri Eshhar <omri@starkware.co>
2024-08-06 13:58:19 +0300

    Fix example test

--- src/bin/run_simple_bootloader/main.rs
+++ src/bin/run_simple_bootloader/main.rs
@@ -4,18 +4,12 @@ use std::io::{self, Write};
 use std::path::PathBuf;
 
 use bincode::enc::write::Writer;
-use cairo_vm::cairo_run::{self, cairo_run_program_with_initial_scope, CairoRunConfig};
-use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::types::layout_name::LayoutName;
-use cairo_vm::types::program::Program;
-use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
-use cairo_vm::vm::runners::cairo_runner::CairoRunner;
 
 use cairo_bootloader::bootloaders::load_simple_bootloader;
+use cairo_bootloader::cairo_run_simple_bootloader_in_proof_mode;
 use cairo_bootloader::tasks::make_bootloader_tasks;
-use cairo_bootloader::{
-    insert_simple_bootloader_input, BootloaderHintProcessor, SimpleBootloaderInput, TaskSpec,
-};
+use cairo_vm::cairo_run;
 use clap::Parser;
 use tempfile::NamedTempFile;
 
@@ -72,47 +66,6 @@ impl FileWriter {
     }
 }
 
-fn cairo_run_simple_bootloader_in_proof_mode(
-    simple_bootloader_program: &Program,
-    tasks: Vec<TaskSpec>,
-    fact_topologies_path: PathBuf,
-    layout: LayoutName,
-) -> Result<CairoRunner, CairoRunError> {
-    let mut hint_processor = BootloaderHintProcessor::new();
-
-    let cairo_run_config = CairoRunConfig {
-        entrypoint: "main",
-        trace_enabled: true,
-        relocate_mem: true,
-        layout,
-        proof_mode: true,
-        secure_run: None,
-        disable_trace_padding: false,
-        allow_missing_builtins: None,
-    };
-
-    // Build the bootloader input
-    let simple_bootloader_input = SimpleBootloaderInput {
-        fact_topologies_path: Some(fact_topologies_path),
-        single_page: true,
-        tasks,
-    };
-
-    // Note: the method used to set the bootloader input depends on
-    // https://github.com/lambdaclass/cairo-vm/pull/1772 and may change depending on review.
-    let mut exec_scopes = ExecutionScopes::new();
-    insert_simple_bootloader_input(&mut exec_scopes, simple_bootloader_input);
-    exec_scopes.insert_value("bootloader_program", simple_bootloader_program.clone());
-
-    // Run the bootloader
-    cairo_run_program_with_initial_scope(
-        simple_bootloader_program,
-        &cairo_run_config,
-        &mut hint_processor,
-        exec_scopes,
-    )
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     let args = match Args::try_parse_from(env::args()) {
         Ok(args) => args,
@@ -137,7 +90,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let runner = cairo_run_simple_bootloader_in_proof_mode(
         &simple_bootloader_program,
         tasks,
-        args.fact_topologies_path,
+        Some(args.fact_topologies_path),
         args.layout,
     )?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+use std::path::PathBuf;
+
+use cairo_vm::cairo_run::{cairo_run_program_with_initial_scope, CairoRunConfig};
 use cairo_vm::types::exec_scope::ExecutionScopes;
+use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::program::Program;
+use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
+use cairo_vm::vm::runners::cairo_runner::CairoRunner;
 
 pub use hints::*;
 
@@ -19,4 +26,45 @@ pub fn insert_simple_bootloader_input(
     simple_bootloader_input: SimpleBootloaderInput,
 ) {
     exec_scopes.insert_value(SIMPLE_BOOTLOADER_INPUT, simple_bootloader_input);
+}
+
+pub fn cairo_run_simple_bootloader_in_proof_mode(
+    simple_bootloader_program: &Program,
+    tasks: Vec<TaskSpec>,
+    fact_topologies_path: Option<PathBuf>,
+    layout: LayoutName,
+) -> Result<CairoRunner, CairoRunError> {
+    let mut hint_processor = BootloaderHintProcessor::new();
+
+    let cairo_run_config = CairoRunConfig {
+        entrypoint: "main",
+        trace_enabled: true,
+        relocate_mem: true,
+        layout,
+        proof_mode: true,
+        secure_run: None,
+        disable_trace_padding: false,
+        allow_missing_builtins: None,
+    };
+
+    // Build the bootloader input
+    let simple_bootloader_input = SimpleBootloaderInput {
+        fact_topologies_path,
+        single_page: true,
+        tasks,
+    };
+
+    // Note: the method used to set the bootloader input depends on
+    // https://github.com/lambdaclass/cairo-vm/pull/1772 and may change depending on review.
+    let mut exec_scopes = ExecutionScopes::new();
+    insert_simple_bootloader_input(&mut exec_scopes, simple_bootloader_input);
+    exec_scopes.insert_value("bootloader_program", simple_bootloader_program.clone());
+
+    // Run the bootloader
+    cairo_run_program_with_initial_scope(
+        simple_bootloader_program,
+        &cairo_run_config,
+        &mut hint_processor,
+        exec_scopes,
+    )
 }
