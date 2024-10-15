0f4bffa6a1c9d77a2ef3dfe44562974f94071578
YairVaknin-starkware <yair.v@starkware.co>
2024-10-15 19:34:05 +0300

    Refactor_crate_for_running_all_cairo_verifiers_and_bootloaders

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,120 @@
-use std::path::PathBuf;
-
 use cairo_vm::cairo_run::{cairo_run_program_with_initial_scope, CairoRunConfig};
+use cairo_vm::types::errors::program_errors::ProgramError;
 use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::types::layout::CairoLayoutParams;
 use cairo_vm::types::layout_name::LayoutName;
 use cairo_vm::types::program::Program;
 use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use cairo_vm::vm::runners::cairo_runner::CairoRunner;
-
 pub use hints::*;
+use std::io::{self, ErrorKind};
 
-pub mod bootloaders;
 mod hints;
 pub mod tasks;
 
-/// Inserts the bootloader input in the execution scopes.
-pub fn insert_bootloader_input(
-    exec_scopes: &mut ExecutionScopes,
-    bootloader_input: BootloaderInput,
-) {
-    exec_scopes.insert_value(BOOTLOADER_INPUT, bootloader_input);
+enum RunMode {
+    CairoRunner {
+        layout: LayoutName,
+        dynamic_layout_params: Option<CairoLayoutParams>,
+    },
+    Validator,
 }
 
-pub fn insert_simple_bootloader_input(
-    exec_scopes: &mut ExecutionScopes,
-    simple_bootloader_input: SimpleBootloaderInput,
-) {
-    exec_scopes.insert_value(SIMPLE_BOOTLOADER_INPUT, simple_bootloader_input);
+impl RunMode {
+    fn create_config(self) -> CairoRunConfig<'static> {
+        match self {
+            RunMode::CairoRunner {
+                layout,
+                dynamic_layout_params,
+            } => CairoRunConfig {
+                entrypoint: "main",
+                trace_enabled: true,
+                relocate_mem: true,
+                layout,
+                proof_mode: true,
+                secure_run: None,
+                disable_trace_padding: false,
+                allow_missing_builtins: None,
+                dynamic_layout_params,
+            },
+            RunMode::Validator => CairoRunConfig {
+                entrypoint: "main",
+                trace_enabled: false,
+                relocate_mem: false,
+                layout: LayoutName::all_cairo,
+                proof_mode: false,
+                secure_run: None,
+                disable_trace_padding: false,
+                allow_missing_builtins: None,
+                dynamic_layout_params: None,
+            },
+        }
+    }
 }
 
-pub fn cairo_run_simple_bootloader_in_proof_mode(
-    simple_bootloader_program: &Program,
-    tasks: Vec<TaskSpec>,
-    fact_topologies_path: Option<PathBuf>,
+pub fn cairo_run_program(
+    program: &Program,
+    program_name: &str,
+    program_input_contents: String,
     layout: LayoutName,
     dynamic_layout_params: Option<CairoLayoutParams>,
 ) -> Result<CairoRunner, CairoRunError> {
     let mut hint_processor = BootloaderHintProcessor::new();
 
-    let cairo_run_config = CairoRunConfig {
-        entrypoint: "main",
-        trace_enabled: true,
-        relocate_mem: true,
-        layout,
-        proof_mode: true,
-        secure_run: None,
-        disable_trace_padding: false,
-        allow_missing_builtins: None,
-        dynamic_layout_params,
-    };
-
-    // Build the bootloader input
-    let simple_bootloader_input = SimpleBootloaderInput {
-        fact_topologies_path,
-        single_page: true,
-        tasks,
-    };
-
-    // Note: the method used to set the bootloader input depends on
-    // https://github.com/lambdaclass/cairo-vm/pull/1772 and may change depending on review.
     let mut exec_scopes = ExecutionScopes::new();
-    insert_simple_bootloader_input(&mut exec_scopes, simple_bootloader_input);
-    exec_scopes.insert_value(
-        BOOTLOADER_PROGRAM_IDENTIFIERS,
-        simple_bootloader_program.clone(),
-    );
 
-    // Run the bootloader
+    let cairo_run_config = if program_name.contains("applicative_bootloader")
+        || program_name.contains("cairo_verifier")
+    {
+        RunMode::Validator.create_config()
+    } else {
+        RunMode::CairoRunner {
+            layout,
+            dynamic_layout_params,
+        }
+        .create_config()
+    };
+
+    // The following attribute is used to make clippy ignore the repeated if-else block. Should be
+    // removed all blocks are implemented.
+    #[allow(clippy::if_same_then_else)]
+    if program_name.contains("simple_bootloader") {
+        let simple_bootloader_input: SimpleBootloaderInput =
+            serde_json::from_str(&program_input_contents).unwrap();
+
+        // Note: the method used to set the bootloader input depends on
+        // https://github.com/lambdaclass/cairo-vm/pull/1772 and may change depending on review.
+        exec_scopes.insert_value(SIMPLE_BOOTLOADER_INPUT, simple_bootloader_input);
+        exec_scopes.insert_value(BOOTLOADER_PROGRAM_IDENTIFIERS, program.clone());
+    } else if program_name.contains("applicative_bootloader") {
+        let io_error = io::Error::new(
+            ErrorKind::Other,
+            format!("Unimplemented program variant: {}", program_name),
+        );
+        return Err(CairoRunError::Program(ProgramError::IO(io_error)));
+    } else if program_name.contains("bootloader") {
+        let io_error = io::Error::new(
+            ErrorKind::Other,
+            format!("Unimplemented program variant: {}", program_name),
+        );
+        return Err(CairoRunError::Program(ProgramError::IO(io_error)));
+    } else if program_name.contains("cairo_verifier") {
+        let io_error = io::Error::new(
+            ErrorKind::Other,
+            format!("Unimplemented program variant: {}", program_name),
+        );
+        return Err(CairoRunError::Program(ProgramError::IO(io_error)));
+    } else {
+        let io_error = io::Error::new(
+            ErrorKind::Other,
+            format!("Unrecognized program variant: {}", program_name),
+        );
+        return Err(CairoRunError::Program(ProgramError::IO(io_error)));
+    }
+
+    // Run the program with the configured execution scopes and cairo_run_config
     cairo_run_program_with_initial_scope(
-        simple_bootloader_program,
+        program,
         &cairo_run_config,
         &mut hint_processor,
         exec_scopes,
