58807b46c3bc8a3e601b57e8275d209d2343de26
YairVaknin-starkware <yair.v@starkware.co>
2024-11-05 18:47:43 +0200

    Complete_verifier_unpacker_applicative_flow

--- src/hints/mod.rs
+++ src/hints/mod.rs
@@ -1,7 +1,10 @@
+mod applicative_bootloader_hints;
 mod bootloader_hints;
+mod cairo_structs;
 mod codes;
 mod execute_task_hints;
 mod fact_topologies;
+mod fri_layer;
 mod hint_processors;
 mod inner_select_builtins;
 mod load_cairo_pie;
@@ -12,12 +15,17 @@ mod simple_bootloader_hints;
 mod types;
 mod utils;
 mod vars;
+mod vector_commitment;
 mod verifier_hints;
+mod verifier_utils;
 
 pub use hint_processors::{BootloaderHintProcessor, MinimalBootloaderHintProcessor};
 pub use types::{
-    BootloaderConfig, BootloaderInput, CairoVerifierInput, PackedOutput, SimpleBootloaderInput,
-    Task, TaskSpec,
+    ApplicativeBootloaderInput, BootloaderConfig, BootloaderInput, CairoVerifierInput,
+    PackedOutput, ProgramWithInput, SimpleBootloaderInput, Task, TaskSpec,
 };
 
-pub use vars::{BOOTLOADER_INPUT, PROGRAM_OBJECT, SIMPLE_BOOTLOADER_INPUT, VERIFIER_PROOF_INPUT};
+pub use vars::{
+    APPLICATIVE_BOOTLOADER_INPUT, BOOTLOADER_INPUT, BOOTLOADER_PROGRAM, COMPONENT_HEIGHT,
+    SIMPLE_BOOTLOADER_INPUT, VERIFIER_PROGRAM, VERIFIER_PROOF_INPUT,
+};
