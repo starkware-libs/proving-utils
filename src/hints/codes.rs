870e62052b1a5522dce2e83d26a2e31ba1bd225c
YairVaknin-starkware <yair.v@starkware.co>
2025-02-16 17:57:57 +0200

    Impl_ecop_hints

--- src/hints/codes.rs
+++ src/hints/codes.rs
@@ -380,3 +380,22 @@ pub const MOCK_CAIRO_VERIFIER_HASH_TO_FP: &str =
 pub const MOCK_CAIRO_VERIFIER_GET_N_STEPS: &str = "n_steps = program_input[\"n_steps\"]";
 
 pub const MOCK_CAIRO_VERIFIER_N_STEPS_TO_AP: &str = "memory[ap] = to_felt_or_relocatable(n_steps)";
+
+pub const SIMPLE_BOOTLOADER_SIMULATE_EC_OP: &str =
+    "from starkware.cairo.lang.builtins.ec.ec_op_builtin_runner import (
+    ec_op_auto_deduction_rule_wrapper,
+)
+ids.new_ec_op_ptr = segments.add()
+ec_op_seg_idx = ids.new_ec_op_ptr.segment_index
+ec_op_cache = {}
+vm_add_auto_deduction_rule(
+    ec_op_seg_idx,
+    ec_op_auto_deduction_rule_wrapper(ec_op_cache=ec_op_cache),
+)";
+
+pub const SIMULATE_EC_OP_FILL_MEM_WITH_BITS_OF_M: &str = "curr_m = ids.m
+for i in range(ids.M_MAX_BITS):
+    memory[ids.m_bit_unpacking + i] = curr_m % 2
+    curr_m = curr_m >> 1";
+
+pub const SIMULATE_EC_OP_ASSERT_FALSE: &str = "assert False, \"ec_op failed.\"";
