5f9b618e627ac205f9bff8dcdb25a6fb25589a31
Tom-StarkWare <tom.r@starkware.co>
2025-04-15 16:03:43 +0300

    Allow stwo_vm_runner to accept program (#142)

--- crates/vm_runner/src/main.rs
+++ crates/vm_runner/src/main.rs
@@ -1,29 +1,37 @@
 use std::path::PathBuf;
 use std::process::ExitCode;
 
+use cairo_program_runner_lib::cairo_run_program;
+use cairo_program_runner_lib::utils::{get_program, get_program_input};
+use cairo_vm::cairo_run;
+use cairo_vm::types::errors::program_errors::ProgramError;
+use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::vm::errors::cairo_run_errors::CairoRunError;
 use clap::Parser;
 use stwo_cairo_adapter::plain::adapt_finished_runner;
 use stwo_cairo_adapter::vm_import::VmImportError;
 use stwo_cairo_adapter::{ExecutionResources, ProverInput};
 use stwo_cairo_utils::binary_utils::run_binary;
-use stwo_cairo_utils::vm_utils::{run_vm, VmArgs, VmError};
 use thiserror::Error;
 use tracing::{span, Level};
 
 /// Command line arguments for stwo_vm_runner.
 /// Example command line (use absolute paths):
 ///     ```
-///     cargo run -r --bin stwo_vm_runner -- --run_from_cairo_pie
-///     --output_path path/to/output --secure_run=true path/to/cairo/pie
+///     cargo run -r --bin stwo_vm_runner -- --program path/to/program --program_input
+///     path/to/input --layout <LayoutName> --output_execution_resources_path path/to/output
 ///     ```
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    #[command(flatten)]
-    vm_args: VmArgs,
-    /// The file path for the output (the adapted execution resources of the VM run).
-    #[structopt(long = "output_path")]
-    output_path: PathBuf,
+    #[clap(long = "program", help = "Path to the compiled program")]
+    program: PathBuf,
+    #[clap(long = "program_input", help = "Path to the program input file.")]
+    program_input: Option<PathBuf>,
+    #[clap(long = "layout")]
+    layout: LayoutName,
+    #[structopt(long = "output_execution_resources_path")]
+    output_execution_resources_path: PathBuf,
 }
 
 #[derive(Debug, Error)]
@@ -32,12 +40,14 @@ enum Error {
     Cli(#[from] clap::Error),
     #[error("Failed to interact with the file system")]
     IO(#[from] std::io::Error),
-    #[error("VM run failed: {0}")]
-    Vm(#[from] VmError),
     #[error("Serialization failed: {0}")]
     Serde(#[from] serde_json::Error),
     #[error("VM import failed: {0}")]
     VmImport(#[from] VmImportError),
+    #[error("ProgramRunner error: {0}")]
+    ProgramRunner(#[from] ProgramError),
+    #[error("Failed executing the program: {0}")]
+    Runner(#[from] CairoRunError),
 }
 
 fn main() -> ExitCode {
@@ -48,19 +58,27 @@ fn run(args: impl Iterator<Item = String>) -> Result<ProverInput, Error> {
     let _span = span!(Level::INFO, "run").entered();
     let args = Args::try_parse_from(args)?;
 
-    // Usually vm_runner runs the VM in non-proof-mode, in which case we don't need
-    // `disable_trace_padding`. If it runs the VM in proof-mode, it should also disable trace
-    // padding as this is the padding relevant for Stwo.
-    let disable_trace_padding = args.vm_args.proof_mode;
-    let cairo_runner = run_vm(&args.vm_args, disable_trace_padding)?;
+    let program = get_program(args.program.as_path())?;
+    let program_input_contents = get_program_input(&args.program_input)?;
+    let cairo_run_config = cairo_run::CairoRunConfig {
+        entrypoint: "main",
+        trace_enabled: true,
+        relocate_mem: true,
+        layout: args.layout,
+        proof_mode: true,
+        secure_run: None,
+        disable_trace_padding: true,
+        allow_missing_builtins: None,
+        dynamic_layout_params: None,
+    };
+    let cairo_runner = cairo_run_program(&program, program_input_contents, cairo_run_config)?;
     let cairo_input = adapt_finished_runner(cairo_runner)?;
 
     let execution_resources = ExecutionResources::from_prover_input(&cairo_input);
     log::info!("Execution resources: {:#?}", execution_resources);
     std::fs::write(
-        args.output_path,
+        args.output_execution_resources_path,
         serde_json::to_string(&execution_resources)?,
     )?;
-
     Ok(cairo_input)
 }
