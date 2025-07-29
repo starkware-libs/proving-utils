e284b27022d50cf545f9acd6ebd71f3cc5507e92
Nitsan Davidovich <nitsan.d@starkware.co>
2025-07-29 17:53:16 +0300

    create inner prove function

--- crates/stwo_run_and_prove/src/main.rs
+++ crates/stwo_run_and_prove/src/main.rs
@@ -1,6 +1,6 @@
 use cairo_air::PreProcessedTraceVariant;
 use cairo_air::utils::ProofFormat;
-use cairo_air::verifier::{CairoVerificationError, verify_cairo};
+use cairo_air::verifier::verify_cairo;
 use cairo_program_runner_lib::cairo_run_program;
 use cairo_program_runner_lib::utils::{get_cairo_run_config, get_program, get_program_input};
 use cairo_vm::types::errors::program_errors::ProgramError;
@@ -10,6 +10,7 @@ use cairo_vm::vm::errors::runner_errors::RunnerError;
 use clap::Parser;
 use serde::Serialize;
 use std::env;
+use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 use stwo_cairo_adapter::ProverInput;
@@ -76,7 +77,8 @@ struct Args {
         long = "n_proof_attempts",
         help = "Number of attempts for proving, in case the proof verification fails.",
         default_value_t = 1,
-        requires = "verify"
+        requires = "verify",
+        value_parser = clap::value_parser!(u16).range(1..)
     )]
     n_proof_attempts: u16,
     #[clap(long = "verify", help = "Should verify the generated proof.")]
@@ -108,8 +110,8 @@ enum StwoRunAndProveError {
     Serializing(#[from] sonic_rs::error::Error),
     #[error(transparent)]
     Proving(#[from] ProvingError),
-    #[error(transparent)]
-    Verification(#[from] CairoVerificationError),
+    #[error("cairo verification failed.")]
+    Verification,
 }
 
 // Implement From<Box<CairoRunError>> manually
@@ -148,6 +150,9 @@ fn main() -> Result<(), StwoRunAndProveError> {
     Ok(())
 }
 
+/// Runs the program and generates a proof for it.
+/// Saves the proof to the specified output dir (may include multiple proofs from repeated
+/// attempts). If `program_output` is provided, saves the program output to that path.
 fn stwo_run_and_prove(
     program: PathBuf,
     program_input: Option<PathBuf>,
@@ -174,8 +179,7 @@ fn stwo_run_and_prove(
     let runner = cairo_run_program(&program, program_input, cairo_run_config)?;
     let mut prover_input_info = runner.get_prover_input_info()?;
     let prover_input = adapter(&mut prover_input_info)?;
-
-    let output_vec = prove(prover_input, prove_args)?;
+    let output_vec = create_params_and_prove(prover_input, prove_args)?;
 
     if let Some(output_path) = program_output {
         save_output_to_file(output_vec, output_path)?;
@@ -184,9 +188,11 @@ fn stwo_run_and_prove(
     Ok(())
 }
 
-/// Prepares the prover parameters, chooses the merkle channel based on the channel hash,
-/// and calls the `prove_with_channel` function to generate the proof.
-fn prove(
+/// Prepares the prover parameters and generates proof given the prover input and parameters.
+/// Verifies the proof in case the respective flag is set.
+/// In case the proof verification fails, it retries up to `n_proof_attempts` times.
+/// Returns the program output.
+fn create_params_and_prove(
     prover_input: ProverInput,
     prove_args: ProveArgs,
 ) -> Result<OutputVec, StwoRunAndProveError> {
@@ -199,76 +205,118 @@ fn prove(
         None => default_prod_prover_parameters(),
     };
 
-    let prove_with_channel_fn = match channel_hash {
-        ChannelHash::Blake2s => prove_with_channel::<Blake2sMerkleChannel>,
-        ChannelHash::Poseidon252 => prove_with_channel::<Poseidon252MerkleChannel>,
-    };
+    // create the directory if it doesn't exist
+    std::fs::create_dir_all(&prove_args.proofs_dir)?;
+    let proof_format = prove_args.proof_format;
 
-    prove_with_channel_fn(prover_input, pcs_config, preprocessed_trace, prove_args)
+    for i in 0..prove_args.n_proof_attempts {
+        let proof_file_path = prove_args.proofs_dir.join(format!("proof_{}", i));
+        let proof_file = create_file(&proof_file_path)?;
+
+        match choose_channel_and_prove(
+            prover_input.clone(),
+            pcs_config,
+            preprocessed_trace,
+            proof_file,
+            &proof_format,
+            channel_hash,
+            prove_args.verify,
+        ) {
+            Ok(output_values) => return Ok(output_values),
+
+            Err(StwoRunAndProveError::Verification) if i < prove_args.n_proof_attempts - 1 => {
+                // Retry on verification error except for the last attempt
+                continue;
+            }
+
+            Err(e) => return Err(e),
+        }
+    }
+
+    panic!("Should not reach here, n_proof_attempts should be at least 1.");
 }
 
-/// Generates proof given the prover input and prover parameters, using the specified merkel
-/// channel. Serializes the proof as cairo-serde or JSON and write to the proof path.
-/// Verifies the proof in case the respective flag is set.
-/// Returns the program output in case the respective flag is set.
-fn prove_with_channel<MC: MerkleChannel>(
+/// Chooses the appropriate channel based on the `channel_hash` and generates a proof.
+fn choose_channel_and_prove(
     prover_input: ProverInput,
     pcs_config: PcsConfig,
     preprocessed_trace: PreProcessedTraceVariant,
-    prove_args: ProveArgs,
+    proof_file: File,
+    proof_format: &ProofFormat,
+    channel_hash: ChannelHash,
+    verify: bool,
+) -> Result<OutputVec, StwoRunAndProveError> {
+    match channel_hash {
+        ChannelHash::Blake2s => prove::<Blake2sMerkleChannel>(
+            prover_input,
+            pcs_config,
+            preprocessed_trace,
+            proof_file,
+            proof_format,
+            verify,
+        ),
+        ChannelHash::Poseidon252 => prove::<Poseidon252MerkleChannel>(
+            prover_input,
+            pcs_config,
+            preprocessed_trace,
+            proof_file,
+            proof_format,
+            verify,
+        ),
+    }
+}
+
+/// Generates a proof for the given prover input and parameters, using the specified Merkle channel.
+/// Serializes the proof as cairo-serde or JSON and write to the proof path.
+/// Verifies the proof if the `verify` flag is set.
+/// Returns the program output.
+fn prove<MC: MerkleChannel>(
+    prover_input: ProverInput,
+    pcs_config: PcsConfig,
+    preprocessed_trace: PreProcessedTraceVariant,
+    mut proof_file: File,
+    proof_format: &ProofFormat,
+    verify: bool,
 ) -> Result<OutputVec, StwoRunAndProveError>
 where
     SimdBackend: BackendForChannel<MC>,
     MC::H: Serialize,
     <MC::H as MerkleHasher>::Hash: CairoSerialize,
 {
-    let mut output_values: OutputVec = Vec::new();
-    // create the directory if it doesn't exist
-    std::fs::create_dir_all(&prove_args.proofs_dir)?;
+    let proof = prove_cairo::<MC>(prover_input, pcs_config, preprocessed_trace)?;
 
-    for i in 0..prove_args.n_proof_attempts {
-        // TODO(nitsan): move the loop content to a separate function.
-        let proof = prove_cairo::<MC>(prover_input.clone(), pcs_config, preprocessed_trace)?;
-        let proof_file_path = prove_args.proofs_dir.join(format!("proof_{}", i));
-        let mut proof_file = create_file(&proof_file_path)?;
-
-        match prove_args.proof_format {
-            ProofFormat::Json => {
-                let serialized = sonic_rs::to_string_pretty(&proof)?;
-                proof_file.write_all(serialized.as_bytes())?;
-            }
-            ProofFormat::CairoSerde => {
-                let mut serialized: Vec<starknet_ff::FieldElement> = Vec::new();
-                CairoSerialize::serialize(&proof, &mut serialized);
-                let hex_strings: Vec<String> = serialized
-                    .into_iter()
-                    .map(|felt| format!("0x{:x}", felt))
-                    .collect();
-                let serialized_hex = sonic_rs::to_string_pretty(&hex_strings)?;
-                proof_file.write_all(serialized_hex.as_bytes())?;
-            }
+    match proof_format {
+        ProofFormat::Json => {
+            let serialized = sonic_rs::to_string_pretty(&proof)?;
+            proof_file.write_all(serialized.as_bytes())?;
         }
-
-        let output_addresses_and_values = proof.claim.public_data.public_memory.output.clone();
-        output_values = output_addresses_and_values
-            .into_iter()
-            .map(|(_, value)| value)
-            .collect();
-
-        if prove_args.verify {
-            if let Err(verify_err) = verify_cairo::<MC>(proof, preprocessed_trace) {
-                if i == prove_args.n_proof_attempts - 1 {
-                    return Err(StwoRunAndProveError::Verification(verify_err));
-                } else {
-                    // Retry proving if verification fails
-                    continue;
-                }
-            } else {
-                // if verification is successful
-                break;
-            }
+        ProofFormat::CairoSerde => {
+            let mut serialized: Vec<starknet_ff::FieldElement> = Vec::new();
+            CairoSerialize::serialize(&proof, &mut serialized);
+            let hex_strings: Vec<String> = serialized
+                .into_iter()
+                .map(|felt| format!("0x{:x}", felt))
+                .collect();
+            let serialized_hex = sonic_rs::to_string_pretty(&hex_strings)?;
+            proof_file.write_all(serialized_hex.as_bytes())?;
         }
     }
+
+    let output_addresses_and_values = proof.claim.public_data.public_memory.output.clone();
+
+    if verify {
+        // We want to map this error to `StwoRunAndProveError::Verification` because we intend to
+        // retry the proof generation in case of a verification failure. In the calling function we
+        // assume this specific error type, so if we don't map it, and the error type returned by
+        // `verify_cairo` changes, it will break the retry logic.
+        verify_cairo::<MC>(proof, preprocessed_trace)
+            .map_err(|_| StwoRunAndProveError::Verification)?;
+    }
+
+    let output_values = output_addresses_and_values
+        .into_iter()
+        .map(|(_, value)| value)
+        .collect();
 
     Ok(output_values)
 }
