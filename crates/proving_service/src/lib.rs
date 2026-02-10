757467db7de443343118e6a963c166412de6cf80
Ilya Lesokhin <ilya@starkware.co>
2026-02-10 09:09:05 +0200

    Add proving_service based on stwo_run_and_prove.
    First step toward an HTTP proving service. Future PRs will evolve this
    crate into a standalone service.

---
+++ crates/proving_service/src/lib.rs
@@ -0,0 +1,55 @@
+use cairo_air::utils::ProofFormat;
+use tracing::{Level, span};
+
+use std::{fs::read_to_string, path::PathBuf};
+use stwo_cairo_adapter::ProverInput;
+use stwo_cairo_prover::prover::prove_cairo;
+use stwo_cairo_prover::stwo::core::vcs_lifted::blake2_merkle::Blake2sM31MerkleChannel;
+
+use circuit_cairo_air::verify::verify_cairo;
+use circuit_prover::prover::prove_circuit;
+pub use stwo_run_and_prove::{
+    ProveConfig, ProverTrait, RunConfig, StwoProverEntryPoint, StwoRunAndProveError,
+    stwo_run_and_prove,
+};
+
+pub struct ProvingServiceEntryPoint;
+
+impl ProverTrait for ProvingServiceEntryPoint {
+    fn create_and_serialize_proof(
+        &self,
+        prover_input: ProverInput,
+        _verify: bool,
+        _proof_path: PathBuf,
+        _proof_format: ProofFormat,
+        proof_params_json: Option<PathBuf>,
+    ) -> Result<(), StwoRunAndProveError> {
+        let proof_params = if let Some(proof_params_json) = proof_params_json {
+            let s = read_to_string(&proof_params_json)
+                .map_err(|e| StwoRunAndProveError::PathIO(e, proof_params_json.clone()))?;
+            serde_json::from_str(&s)
+                .map_err(|e| StwoRunAndProveError::Anyhow(anyhow::Error::from(e)))?
+        } else {
+            panic!("Proof parameters JSON file is required");
+        };
+
+        let span = span!(Level::INFO, "proving cairo").entered();
+        let cairo_proof = prove_cairo::<Blake2sM31MerkleChannel>(prover_input, proof_params)
+            .map_err(|e| StwoRunAndProveError::Anyhow(anyhow::Error::from(e)))?;
+        span.exit();
+
+        println!("finished proving");
+
+        let span = span!(Level::INFO, "building verification context").entered();
+        let mut context = verify_cairo(&cairo_proof).unwrap();
+        span.exit();
+
+        let span = span!(Level::INFO, "proving verification circuit").entered();
+        let _proof = prove_circuit(&mut context);
+        span.exit();
+
+        // TODO: Serialize the proof to a file.
+
+        Ok(())
+    }
+}
