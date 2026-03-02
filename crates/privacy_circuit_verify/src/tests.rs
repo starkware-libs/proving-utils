acea4e78395113e083e66c8b13064d916ec5a85e
anatg <anatg@starkware.co>
2026-03-02 17:24:07 +0200

    Add privacy prove and verify

---
+++ crates/privacy_circuit_verify/src/tests.rs
@@ -0,0 +1,15 @@
+use circuit_cairo_air::all_components::all_components;
+use stwo::core::fields::qm31::QM31;
+
+use crate::consts::COMPONENTS;
+
+#[test]
+fn check_components() {
+    let all_components = all_components::<QM31>();
+    for component_name in COMPONENTS {
+        assert!(
+            all_components.contains_key(component_name),
+            "Component {component_name} is not in the all_components"
+        );
+    }
+}
---
+++ crates/privacy_prove/src/tests.rs
@@ -0,0 +1,23 @@
+#[cfg(feature = "slow-tests")]
+pub mod slow_tests {
+    use std::path::PathBuf;
+
+    use cairo_vm::vm::runners::cairo_pie::CairoPie;
+    use privacy_circuit_verify::verify;
+    use tracing_subscriber::fmt;
+
+    use crate::privacy_prove;
+
+    #[test]
+    fn test_privacy_prove_and_verify() {
+        let _ = fmt().with_max_level(tracing::Level::INFO).try_init();
+
+        let project_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let pie_path = project_dir.join("test_data/privacy_tx_cairo_pie.zip");
+        let pie = CairoPie::read_zip_file(&pie_path).unwrap();
+
+        // Prove and verify
+        let (proof, output_preimage) = privacy_prove(pie).unwrap();
+        verify(&proof, &output_preimage).unwrap();
+    }
+}
