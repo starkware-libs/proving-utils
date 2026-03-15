4300a460344ee19141ece29741be9fbd1d549722
anatg <anatg@starkware.co>
2026-03-15 15:34:26 +0200

    Add privacy recursive prove and verify

--- crates/privacy_circuit_verify/src/tests.rs
+++ crates/privacy_circuit_verify/src/tests.rs
@@ -1,7 +1,18 @@
+use circuit_air::statement::{
+    INTERACTION_POW_BITS as CIRCUIT_INTERACTION_POW_BITS, all_circuit_components,
+};
+use circuit_air::verify::CircuitConfig;
 use circuit_cairo_air::all_components::all_components;
+use circuits_stark_verifier::proof::ProofConfig;
 use stwo::core::fields::qm31::QM31;
+use stwo::core::pcs::PcsConfig;
 
-use crate::consts::PRIVACY_TRANSACTION_COMPONENTS;
+use crate::consts::{
+    CIRCUIT_FRI_CONFIG, PRIVACY_CAIRO_VERIFIER_CONSTS_HASH,
+    PRIVACY_RECURSION_CIRCUIT_PREPROCESSED_ROOT, PRIVACY_TRANSACTION_COMPONENTS,
+};
+use crate::{build_cairo_verifier_circuit, get_cairo_verifier_config, get_preprocessed_circuit};
+use circuits::ivalue::IValue;
 
 #[test]
 fn check_components() {
@@ -12,4 +23,52 @@ fn check_components() {
             "Component {component_name} is not in the all_components"
         );
     }
+}
+
+#[test]
+fn check_cairo_circuit_verifier_constants() {
+    let cairo_verifier_config = get_cairo_verifier_config().unwrap();
+    let novalue_context = build_cairo_verifier_circuit(&cairo_verifier_config);
+    let constants = novalue_context
+        .constants()
+        .keys()
+        .cloned()
+        .collect::<Vec<_>>();
+    let constants_hash = QM31::blake(constants.as_slice(), constants.len() * 16);
+
+    assert_eq!(constants_hash, PRIVACY_CAIRO_VERIFIER_CONSTS_HASH.into());
+}
+
+#[test]
+fn check_circuit_verifier_configs() {
+    let cairo_verifier_config = get_cairo_verifier_config().unwrap();
+    let preprocessed_circuit = get_preprocessed_circuit(&cairo_verifier_config);
+
+    // compute the circuit config
+    let lifting_log_size =
+        preprocessed_circuit.params.trace_log_size + CIRCUIT_FRI_CONFIG.log_blowup_factor;
+    let circuit_pcs_config = PcsConfig {
+        pow_bits: CIRCUIT_INTERACTION_POW_BITS,
+        fri_config: CIRCUIT_FRI_CONFIG,
+        lifting_log_size: Some(lifting_log_size),
+    };
+    let _circuit_config = CircuitConfig {
+        config: circuit_pcs_config,
+        output_addresses: preprocessed_circuit.params.output_addresses.clone(),
+        n_blake_gates: preprocessed_circuit.params.n_blake_gates,
+        preprocessed_column_ids: preprocessed_circuit.preprocessed_trace.ids(),
+        preprocessed_root: PRIVACY_RECURSION_CIRCUIT_PREPROCESSED_ROOT.into(),
+    };
+
+    // compute the proof config
+    let _proof_config = ProofConfig::from_components(
+        &all_circuit_components::<QM31>(),
+        preprocessed_circuit.preprocessed_trace.ids().len(),
+        &circuit_pcs_config,
+        CIRCUIT_INTERACTION_POW_BITS,
+    );
+
+    // TODO(AnatG): Uncomment these when stwo version is updated in stwo-circuits.
+    // assert_eq!(circuit_config, get_circuit_config());
+    // assert_eq!(proof_config, get_proof_config());
 }
