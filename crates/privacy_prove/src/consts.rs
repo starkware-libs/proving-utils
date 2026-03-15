cbaaee511b89d0bd4d2945dcfdb9488a35298e7e
anatg <anatg@starkware.co>
2026-03-15 15:34:26 +0200

    Add privacy recursive prove and verify

--- crates/privacy_prove/src/consts.rs
+++ crates/privacy_prove/src/consts.rs
@@ -1,7 +1,7 @@
 use cairo_air::PreProcessedTraceVariant;
 use cairo_vm::cairo_run::CairoRunConfig;
 use cairo_vm::types::layout_name::LayoutName;
-use privacy_circuit_verify::consts::PCS_CONFIG;
+use privacy_circuit_verify::consts::CAIRO_PCS_CONFIG;
 use stwo_cairo_prover::prover::{ChannelHash, ProverParameters};
 
 pub const CAIRO_RUN_CONFIG: CairoRunConfig<'_> = CairoRunConfig {
@@ -18,11 +18,13 @@ pub const CAIRO_RUN_CONFIG: CairoRunConfig<'_> = CairoRunConfig {
     allow_missing_builtins: Some(true),
 };
 
-pub const PROVER_PARAMS: ProverParameters = ProverParameters {
+pub const CAIRO_PROVER_PARAMS: ProverParameters = ProverParameters {
     channel_hash: ChannelHash::Blake2sM31,
-    pcs_config: PCS_CONFIG,
+    pcs_config: CAIRO_PCS_CONFIG,
     preprocessed_trace: PreProcessedTraceVariant::CanonicalSmall,
     channel_salt: 0,
     store_polynomials_coefficients: true,
     include_all_preprocessed_columns: true,
 };
+
+pub const CIRCUIT_STORE_POLYNOMIALS_COEFFICIENTS: bool = true;
