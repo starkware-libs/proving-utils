24a2edb1a9ce37c7ada494075b6dcb12a6d4c32e
Omri Eshhar <omri@starkware.co>
2026-07-20 14:48:32 +0300

    prove_leaf: assert raise_min_lifting_to_max_column=true

--- crates/leaf_prover/src/prove_leaf.rs
+++ crates/leaf_prover/src/prove_leaf.rs
@@ -45,6 +45,10 @@ pub fn prove_leaf(
         cairo_prover_parameters.include_all_preprocessed_columns,
         "The prover parameters must set include_all_preprocessed_columns=true because the verifier circuit expects a constant number of preprocessed columns"
     );
+    assert!(
+        cairo_prover_parameters.raise_min_lifting_to_max_column,
+        "The prover parameters must set raise_min_lifting_to_max_column=true"
+    );
 
     // Execute & prove the input Cairo program.
 
